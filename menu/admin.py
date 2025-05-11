3b10e1a0b9322d392a85c3d101d818dee0e26a02
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-05-11 11:32:12 +0100

    Remove box behind create your own section. Make option boxes sit horizontally. Fix CSS issues with layout.

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Menu
 
-# Register your models here.
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    """
+    Class to display menu items
+    """
+    list_display = ('name', 'description', 'price', 'is_available')
+    list_filter = ('is_available',)
