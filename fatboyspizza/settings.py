253e94c804b757632bc9a544d40c14a54e544ceb
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-05-16 13:33:42 +0100

    Add localhost and heroku url to allowed hosts in settings

--- fatboyspizza/settings.py
+++ fatboyspizza/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [127.o.o.1, 'localhost', 'fatboyspizza.herokuapp.com']
 
 
 # Application definition
