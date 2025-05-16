26f0ad6d5c3e8616298c24923bed860719952c59
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-05-16 13:48:45 +0100

    Fix incorrect url in allowed hosts

--- fatboyspizza/settings.py
+++ fatboyspizza/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fatboyspizza.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'fat-boys-pizza.herokuapp.com']
 
 
 # Application definition
