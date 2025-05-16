4b7ce12ef3c1c4a0d9986d6c7b0bed22dcd9282a
Luke-Nicklin <u88xtjl9@students.codeinstitute.net>
2025-05-16 14:00:27 +0100

    Add whitenoise to settings

--- fatboyspizza/settings.py
+++ fatboyspizza/settings.py
@@ -175,6 +175,8 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Cloudinary settings
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 CLOUDINARY_URL = os.environ.get('CLOUDINARY_URL')
